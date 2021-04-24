ba4a1e8fffacf6c75de89733aec21ddbe38d6978
Fer-Romero96 <A01335834@itesm.mx>
2021-04-24 12:45:46 -0500

    se agregaron archivos

--- app/src/main/java/edu/itesm/pokedex/LoginActivity.kt
+++ app/src/main/java/edu/itesm/pokedex/LoginActivity.kt
@@ -18,6 +18,7 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
